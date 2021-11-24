ca3c805b8abfb1770c5113b94fdca839a6b953fb
llz1990 <754960880@qq.com>
2021-11-24 14:34:49 +0800

    初始化工程

--- common/util.js
+++
@@ -1,8 +0,0 @@
-class Utils {
-    static getObjectData(data){
-        if(typeof data !== 'object') return false;
-        return  JSON.parse(JSON.stringify(data));
-    }
-}
-
-module.exports = Utils;
