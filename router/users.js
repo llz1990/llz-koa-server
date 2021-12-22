e37605825f66022c8c2c622ae18c3607580b78d6
llz1990 <754960880@qq.com>
2021-12-22 23:55:33 +0800

    初始化原始项目

--- router/users.js
+++ router/users.js
@@ -9,7 +9,7 @@ const ResHandleObj = require('../common/resHandle');
 const Validate = require('../common/Validate');
 const crypto = require("crypto");
 
-// 注册用户信息：
+// 注册用户信息:
 users.post('/register', async (ctx, next) => {
   const { name, password, checkpwd } = ctx.request.body;
   const validate = Validate.registerCheck({ name, password, checkpwd });
@@ -23,7 +23,7 @@ users.post('/register', async (ctx, next) => {
   const passwordMd5 = md5.update(password).digest("hex");
   try {
     const sqlData = await model.registerData(name, passwordMd5);
-    ctx.body = new ResHandleObj(0, sqlData);
+    ctx.body = new ResHandleObj(200, sqlData);
   } catch (err) {
     ctx.throw(400, '用户注册失败');
   }
@@ -32,15 +32,16 @@ users.post('/register', async (ctx, next) => {
 // 用户登陆之前验证用户是否处于登陆状态（session 是否过期）
 users.get('/checkIsLogin', async (ctx, next) =>{
   if (ctx.session && ctx.session.user) {
-    ctx.body = new ResHandleObj(0, '用户处于登录状态');
+    ctx.body = new ResHandleObj(200, '用户处于登录状态');
   } else {
-    ctx.body = new ResHandleObj(-1, '当前用户没有登录');
+    ctx.body = new ResHandleObj(400, '当前用户没有登录');
   }
 })
 
 // 用户登录：
 users.post('/login', async (ctx, next) => {
   const { name, password } = ctx.request.body;
+  console.log(ctx.request.body)
   const validate = Validate.loginCheck({ name, password });
   if (validate) {
     ctx.throw(400, '参数错误!');
@@ -51,16 +52,16 @@ users.post('/login', async (ctx, next) => {
     ctx.session = {
       user: name
     }
-    ctx.body = new ResHandleObj(0, sqlData);
+    ctx.body = new ResHandleObj(200, sqlData);
   } else {
-    ctx.body = new ResHandleObj(-1, `登陆账户或密码错误`);
+    ctx.body = new ResHandleObj(400, `登陆账户或密码错误`);
   }
 })
 
 // 账号退出登录
 users.post('/logout', async (ctx) => {
   ctx.session = null;  // 清空session
-  ctx.body = new ResHandleObj(0, `用户退出登录`);
+  ctx.body = new ResHandleObj(200, `用户退出登录`);
 });
 
 module.exports = users
