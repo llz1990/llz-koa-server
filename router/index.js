fc75448d2408173f2873e129aea2b9b651bdf123
llz1990 <754960880@qq.com>
2022-01-19 14:15:25 +0800

    添加接口测试api

--- router/index.js
+++ router/index.js
@@ -10,5 +10,16 @@ const users = require('./users');
 router.use(list.routes(), list.allowedMethods());     // 列表相关
 router.use(users.routes(), users.allowedMethods());   // 用户信息相关
 
+/**
+ * 取所有相册合集
+ */
+ router.get('/test', async(ctx , next) => {
+    try {
+      ctx.body = { code: 200, data: '接口测试成功' };
+    } catch (err) {
+      ctx.throw(400, '接口测试失败');
+    }
+  })
+
 module.exports = router;
 
